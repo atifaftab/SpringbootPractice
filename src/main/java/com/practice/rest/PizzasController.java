82cb2bd2f8f7381f13c0784510f8770b1877f226
Atif <iamatif96@gmail.com>
2024-03-18 21:23:59 +0800

    new controller

---
+++ src/main/java/com/practice/rest/PizzasController.java
@@ -0,0 +1,17 @@
+package com.practice.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PizzasController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+//    @GetMapping
+    public void get() {
+        System.out.println("server : "+serverPort);
+    }
+}
